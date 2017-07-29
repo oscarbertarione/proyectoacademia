61224eb4976fa8f4a74d7d61cada3893d1f319a8
Oscar <oscarbertarione@gmail.com>
2017-07-29 02:35:06 -0400

    formularionewregistrations

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'rails', '~> 5.1.2'
 gem 'devise'
 gem 'twitter-bootstrap-rails'
 gem 'devise-bootstrap-views'
+gem 'stripe'
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
 # Use SCSS for stylesheets
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170729011414) do
+ActiveRecord::Schema.define(version: 20170729062331) do
 
   create_table "apuntes", force: :cascade do |t|
     t.text "content"
@@ -51,6 +51,14 @@ ActiveRecord::Schema.define(version: 20170729011414) do
   end
 
   create_table "notes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "payments", force: :cascade do |t|
+    t.string "email"
+    t.string "token"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
