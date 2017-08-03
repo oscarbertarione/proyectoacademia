5a9b434fed71bb68d88927f774d93d5abe12d303
oscarhuaweip2 <oscarhuaweip2@gmail.com>
2017-08-03 23:39:35 +0000

    prueba pago en keroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   config.eager_load = true
 
   config.action_mailer.delivery_method = :smtp 
-config.action_mailer.default_url_options = { :host => 'https://proyectoacademia.herokuapp.com'}
+config.action_mailer.default_url_options = { :host => 'https://proyectoacademiafinal.herokuapp.com'}
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
