9ad3e08add74e3dc3a31006ef6570159df14dbc7
oscarhuaweip2 <oscarhuaweip2@gmail.com>
2017-11-19 19:07:58 +0000

    cambio

--- app/controllers/dashboard_controller.rb
+++
@@ -1,8 +0,0 @@
-class DashboardController < ApplicationController
-  def index
-  	@cursos = Curso.all
-    @leccions = Leccion.all
-    @apuntes = Apunte.all
-    @leccion = Leccion.find(1)
-  end
-end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
 	skip_before_action :authenticate_user!, only: [:index]
   def index
-
+  	@home = Home.find(1)
+  	@cursos = Curso.find(1)
   end
 end
