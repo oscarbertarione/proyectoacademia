669786ca2d861891bd45dbc5ef0e15d84a7ffa63
oscarhuaweip2 <oscarhuaweip2@gmail.com>
2017-11-19 17:50:35 +0000

    cambio

--- app/controllers/curso_frontend_controller.rb
+++ app/controllers/curso_frontend_controller.rb
@@ -1,13 +1,10 @@
 class CursoFrontendController < ApplicationController
 	skip_before_action :authenticate_user!, only: [:index, :show]
   def index
-  	@curso = CursoFrontend.find(1)
-  	@cursos = Curso.all
-  	@home = Home.find(1)
+
   end
 
   def show
-  	@curso = Curso.find(params[:id])
-  	@home = Home.find(1)
+
   end
 end
